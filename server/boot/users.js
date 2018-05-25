97906fff703055be46dcc47e0c6d1cf803d9cbe1
guildenstern <alessiosaltarin@it.ibm.com>
2018-05-25 16:30:21 +0200

    Access Token

--- server/boot/users.js
+++ server/boot/users.js
@@ -1,13 +1,22 @@
 'use strict';
 
 module.exports = function(app) {
-  let User = app.models.User;
+
+  let User = app.models.AppUser;
+
   User.create({email: 'admin@fakepagopa.it', password: 'admin'},
     function(err, userInstance) {
+      if (err)
+        console.error(JSON.stringify(err));
       console.log('Created User > ' + JSON.stringify(userInstance));
     });
+
   User.create({email: 'alessio@fakepagopa.it', password: 'doctor'},
     function(err, userInstance) {
+      if (err)
+          console.error(JSON.stringify(err));
       console.log('Created User > ' + JSON.stringify(userInstance));
     });
+
+
 };
